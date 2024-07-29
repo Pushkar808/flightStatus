1b04439a4a0baecfc88e1c2b98f7e3221cc8574f
Pushkar gupta <pushkargupta808@gmail.com>
2024-07-30 00:55:12 +0530

    added sockets

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Splash } from './pages/Splash';
-
+import { io } from 'socket.io-client';
+import { useEffect, useState } from 'react';
 function App() {
+  const socket = io('http://localhost:5000'); // Connect to the server
+  const [notifications, setNotifications] = useState([]);
+  useEffect(() => {
+    // Listen for notification events from the server
+    socket.on('notification', (notification) => {
+      console.log("Got a socket with data", notification)
+      setNotifications((prevNotifications) => [...prevNotifications, notification]);
+    });
+    socket.on('message', (notification) => {
+      console.log("Got a socket with data", notification)
+    })
+    // Clean up on component unmount
+    return () => {
+      socket.off('notification');
+    };
+  }, []);
+
   return (
     <>
       <Routes>
